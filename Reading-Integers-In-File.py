a5d7cd70ce79802c6e9eeb8f47ec094a1e2ab1bc
jayceee1207 <ablayjc7@gmail.com>
2023-04-24 19:56:22 +0800

    Pseudocode

--- Reading-Integers-In-File.py
+++ Reading-Integers-In-File.py
@@ -2,3 +2,20 @@
 #BSCPE 1-5
 #Assignment 4: Program 4
 #April 22, 2023
+
+#PSEUDOCODE
+#Make a file where we will store random numbers
+#Read every line of numbers
+#Create a list where we store all even numbers
+#Create a list where we store all odd numbers
+#Write a for loop for numbers in number.txt file
+#for number in number.txt file
+#   convert numbers into integer
+#   if integer is even number
+#       get the square of the even number 
+#       we will append that number into the even numbers list
+#   if integer is odd number
+#       get the cube of the odd number 
+#       we will append that number into the odd numbers list
+#open a file to store all numbers in even numbers list
+#open a file to store all numbers in even numbers list
